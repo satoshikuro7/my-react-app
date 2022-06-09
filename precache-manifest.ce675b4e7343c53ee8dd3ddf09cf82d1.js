a4d3b3ccf5c142fc6f5c9b7ddadf4711a7e2b1ba
satoshikuro7 <satoshikuro7@gmail.com>
2022-06-09 18:56:53 +0530

    Updates

---
+++ precache-manifest.ce675b4e7343c53ee8dd3ddf09cf82d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b03dd136dcc40aef59f2",
+    "url": "/my-react-app/static/css/main.60fb9a9b.chunk.css"
+  },
+  {
+    "revision": "b03dd136dcc40aef59f2",
+    "url": "/my-react-app/static/js/main.5cd58857.chunk.js"
+  },
+  {
+    "revision": "e42ac3f54c66b244f273",
+    "url": "/my-react-app/static/js/runtime~main.acc6ab88.js"
+  },
+  {
+    "revision": "ee22aa6e562cbf919bdb",
+    "url": "/my-react-app/static/js/2.f138eb6d.chunk.js"
+  },
+  {
+    "revision": "c0c98a6c240aa4e0d888",
+    "url": "/my-react-app/static/js/3.68b36b5f.chunk.js"
+  },
+  {
+    "revision": "9af63721f2be4f5228711f2cafb8a925",
+    "url": "/my-react-app/index.html"
+  }
+];
